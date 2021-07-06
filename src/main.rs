a5fae635b5dff32e8b9369491e47e11605874623
Luke <luke@yorkbay.net.nz>
2021-07-06 16:09:27 +1200

    rewrote main, working on buffer manipulations

--- src/main.rs
+++ src/main.rs
@@ -13,120 +13,49 @@ use crossterm::{
     MoveToColumn, MoveToRow, position},
 };
 
-//const U8TOCHAR: [StyledContent<char>; 4] = [' '.on_blue(), '.'.on_green(), '@'.on_blue(), '#'.on_blue()];
+/*
+    NOTES TO LUKE: 
+        - All coords (column, row)
+        - Bufs and Vec of columns, each rows number long
 
-fn reset_screen(x: u16, y: u16) {
-    execute!(
-        stdout(),
-        Clear(All),
-        MoveToColumn(x),
-        MoveToRow(y),
-    );
-}
-
-fn display_buffer(buf: &Vec<Vec<u8>>) {
-    let u8_to_char: [StyledContent<char>; 4] = [' '.on_blue(), '.'.on_green(), '@'.on_blue(), '#'.on_blue()];
-
-    reset_screen(0, 0);
-
-    let mut copy = buf.clone();
-
-    copy[0][0] = 1;
-    copy[0][3] = 1;
-    copy[0][4] = 1;
-    copy[0][5] = 1;
-    
-    for row in copy.iter() {
-        for c in row.iter() {
-            //execute!(stdout(), Print(u8_to_char[*c as usize]));
-            execute!(stdout(), Print(*c));
-        }
-    }
-}
-
-fn buffer_to_view(buf: Vec<Vec<u8>>, top_left: (usize, usize), bottom_right: (usize, usize)) -> Vec<Vec<u8>> {
-    let mut view: Vec<Vec<u8>> = Vec::new();
-
-    let iter = buf[top_left.1..bottom_right.1].iter().map(|s| &s[top_left.0..bottom_right.0]);
-
-    for row in iter {
-        let mut new_row: Vec<u8> = Vec::new();
-        for x in row {
-            new_row.push(*x);
-        }
-        view.push(new_row);
-    }
-
-    return view;
-}
-
-fn get_initial_buffer() -> Vec<Vec<u8>> {
-    let mut result = vec![vec![0; 100]; 300];
-
-    /*for (idx1, x) in result.clone().iter().enumerate() {
-        for (idx2, y) in x.iter().enumerate() {
-            if y % 30 == 0 {
-                result[idx1][idx2] = 1;
-            }
-        }
-    }*/
-
-    result[0][2] = 1;
-    result[0][3] = 1;
-    result[0][4] = 1;
-    result[0][5] = 1;
-    //result[20][20] = 2;
-    //result[20][21] = 2;
-    //result[21][21] = 2;
-    //result[21][20] = 2;
-
-    return result;
-}
-
+*/
 fn main() -> Result<()> {
-    execute!(stdout(), SetSize(100, 300))?;
-
-    let (cols, rows): (u16, u16) = (100, 300);//size()?;
-
-    let mut buf: Vec<Vec<u8>> =  vec![vec![0; cols.into()]; rows.into()]; //get_initial_buffer();
-
-    buf[0][2] = 1;
-    buf[0][3] = 1;
-    buf[0][4] = 1;
-    buf[0][5] = 1;
-
-    /*let mut new_buf: Vec<Vec<u8>> = Vec::new(); //vec![vec![0; rows.into()]; cols.into()];
-
-    for (index, row) in buf.clone().iter().enumerate() {
-        new_buf.push(Vec::new());
-        for (idx, c) in row.iter().enumerate() {
-            if index < 95 {
-                new_buf[index].push(0);
-            } else {
-                new_buf[index].push(buf[index][idx]);
-            }
-        }
-    }*/
-
     enable_raw_mode()?;
 
-    execute!(stdout(), EnterAlternateScreen)?;
-
     execute!(
         stdout(),
+        EnterAlternateScreen,
         SetBackgroundColor(Color::Blue),
-        SetForegroundColor(Color::Red),
-        //MoveToColumn(0),
-        //MoveToRow(0),
+        SetForegroundColor(Color::Black),
     )?;
 
-    display_buffer(/*buffer_to_view(*/&buf/*, (0, 0), (100, 300))*/);
+    let mut state = match State::new((80, 40)) { // columns, rows
+        Ok(state) => state,
+        Err(err) => panic!("{}", err),
+    };
 
-    //reset_screen(0,0);
+    state.draw_buffer();
 
-    //execute!(stdout(), Print(buf[0][1]), Print(buf[0][0]));
+    let mut x: i32 = 0;
 
-    let mut x = 0;
+    match read()? {
+        _ => x += 1,
+    }
+
+    state.update_char_at_index((0, 0), 1);
+    state.update_char_at_index((0, 1), 1);
+    state.update_char_at_index((1, 0), 1);
+    state.update_char_at_index((1, 1), 1);
+
+    let matrix: Vec<Vec<u8>> = vec![
+        vec![1, 1],
+        vec![1, 1],
+    ];
+
+    //state.insert_matrix_at_index((20, 20), matrix);
+    state.update_use_buffer();
+
+    state.draw_buffer();
 
     match read()? {
         _ => x += 1,
@@ -135,4 +64,91 @@ fn main() -> Result<()> {
     disable_raw_mode()?;
 
     execute!(stdout(), ResetColor, LeaveAlternateScreen)
+}
+
+struct State {
+    buffer: Vec<Vec<u8>>,
+    alternate_buffer: Vec<Vec<u8>>,
+    dimensions: (u16, u16), // columns, rows
+    style_map: [StyledContent<char>; 4],
+}
+
+impl State {
+    fn new(dimensions: (u16, u16)) -> Result<State> {
+        execute!(
+            stdout(),
+            SetSize(dimensions.0, dimensions.1), // columns, rows
+            Clear(All),
+            MoveToColumn(0),
+            MoveToRow(0),
+        )?;
+
+        let buffer = vec![vec![0; 40]; 80]; 
+        // vec of colums, with rows number of elements
+        let alternate_buffer = buffer.clone();
+        let style_map = [' '.on_blue(), '.'.on_green(), '@'.on_blue(), '#'.on_blue()];
+
+        Ok(State {
+            buffer,
+            alternate_buffer,
+            dimensions,
+            style_map,
+        })
+    }
+
+    fn draw_buffer(&mut self) -> Result<()> {
+        execute!(
+            stdout(),
+            Clear(All),
+            MoveToColumn(0),
+            MoveToRow(0),
+        )?;
+
+        let mut view: Vec<Vec<u8>> = Vec::new();
+
+        let (columns, rows) : (usize, usize) = (self.dimensions.0.into(), self.dimensions.1.into());
+
+        let iter = self.alternate_buffer[0..columns]
+                    .iter()
+                    .map(|s| &s[0..rows]);
+
+        for column in iter {
+            view.push((*column).to_vec());
+        }
+
+        for row in view {
+            for c in row {
+                let character: StyledContent<char> = self.style_map[c as usize];
+                execute!(stdout(), Print(character))?; // add mapping
+            }
+        }
+
+        Ok(())
+    }
+
+    fn update_char_at_index(&mut self, location: (u16, u16), value: u8) {
+        // use: (column, row) value
+        let (column, row) : (usize, usize) = (location.0.into(), location.1.into());
+
+        self.buffer[column][row] = value;
+    }
+
+    fn insert_matrix_at_index(&mut self, location: (u16, u16), matrix: Vec<Vec<u8>>) {
+        let (column, row) : (usize, usize) = (location.0.into(), location.1.into());
+
+        /*for (x, col) in matrix.iter().enumerate() {
+            for (y, value) in col.iter().enumerate() {
+                self.buffer[column + x][row] = *value;
+            }
+        }*/
+
+        self.buffer[column][row] = matrix[0][0];
+        //self.buffer[column][row+1] = matrix[0][1];
+        self.buffer[column+1][row] = matrix[0][0];
+        //self.buffer[column+1][row+1] = matrix[1][1];
+    }
+
+    fn update_use_buffer(&mut self) {
+        self.alternate_buffer = self.buffer.clone();
+    }
 }
