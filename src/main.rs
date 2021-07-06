d0458b1398833b24406b790921b70635d24612c7
Luke <luke@yorkbay.net.nz>
2021-07-07 00:05:41 +1200

    optimised buffer drawing, added rudimentary event handling

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 use std::io::{stdout, Write};
+use std::time::Duration;
 
 use crossterm::{
     execute,
     style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor,
     Stylize, StyledContent},
     ExecutableCommand, Result,
-    event,
+    event, 
     terminal::{EnterAlternateScreen, LeaveAlternateScreen, SetSize, size,
     disable_raw_mode, enable_raw_mode, Clear, ClearType::{All}},
-    event::{read, Event},
+    event::{read, Event, poll, KeyCode},
     cursor::{MoveUp, MoveDown, MoveLeft, MoveRight,
-    MoveToColumn, MoveToRow, position},
+    MoveToColumn, MoveToRow, position, Hide, Show},
 };
 
 /*
@@ -25,6 +26,7 @@ fn main() -> Result<()> {
     execute!(
         stdout(),
         EnterAlternateScreen,
+        Hide,
         SetBackgroundColor(Color::Blue),
         SetForegroundColor(Color::Black),
     )?;
@@ -34,7 +36,7 @@ fn main() -> Result<()> {
         Err(err) => panic!("{}", err),
     };
 
-    state.draw_buffer()?;
+    state.draw_buffer_init()?;
 
     let mut x: i32 = 0;
 
@@ -56,15 +58,80 @@ fn main() -> Result<()> {
     state.insert_matrix_at_index((20, 20), matrix);
     state.update_use_buffer();
 
-    state.draw_buffer()?;
+    state.draw_buffer_init()?;
 
     match read()? {
         _ => x += 1,
     }
 
+    let (mut x, mut y) = (1, 0);
+    let (mut old_x, mut old_y) = (0, 0);
+
+    loop {
+        state.update_char_at_index((x, y), 2);
+        state.update_char_at_index((old_x, old_y), 0);
+
+        state.draw_buffer()?;
+
+        match is_event_availble() {
+            Ok(true) => {
+                match read()? {
+                    Event::Key(key_event) => {
+                        match key_event.code {
+                            KeyCode::Char(c) => {
+                                match c {
+                                    'q' => break,
+                                    _ => continue,
+                                }
+                            },
+                            KeyCode::Right => {
+                                old_x = x;
+                                old_y = y;
+                                x += 1;
+                            },
+                            KeyCode::Left => {
+                                old_x = x;
+                                old_y = y;
+                                x -= 1;
+                            },
+                            KeyCode::Up => {
+                                old_x = x;
+                                old_y = y;
+                                y -= 1;
+                            },
+                            KeyCode::Down => {
+                                old_x = x;
+                                old_y = y;
+                                y += 1;
+                            },
+                            _ => continue,
+                        }
+                    },
+                    _ => continue,
+                }
+            },
+            _ => continue,
+        }
+    }
+
     disable_raw_mode()?;
 
-    execute!(stdout(), ResetColor, LeaveAlternateScreen)
+    execute!(stdout(), ResetColor, LeaveAlternateScreen, Show)
+}
+
+fn is_event_availble() -> Result<bool> {
+    poll(Duration::from_secs(0))
+}
+
+fn get_sub_view(buffer: &Vec<Vec<u8>>, location: (u16, u16), width: usize, height: usize) -> Vec<Vec<u8>> {
+    let (column, row) : (usize, usize) = (location.0.into(), location.1.into());
+    
+    let view: Vec<Vec<u8>> = buffer[column..(column + width)]
+                            .iter()
+                            .map(|s| s[row..(row + height)].to_vec())
+                            .collect();
+
+    return view;
 }
 
 struct State {
@@ -97,25 +164,17 @@ impl State {
         })
     }
 
-    fn draw_buffer(&mut self) -> Result<()> {
+    fn draw_buffer_init(&mut self) -> Result<()> {
         execute!(
             stdout(),
             Clear(All),
             MoveToColumn(0),
             MoveToRow(0),
         )?;
-
-        let mut view: Vec<Vec<u8>> = Vec::new();
-
+        
         let (columns, rows) : (usize, usize) = (self.dimensions.0.into(), self.dimensions.1.into());
 
-        let iter = self.alternate_buffer[0..columns]
-                    .iter()
-                    .map(|s| &s[0..rows]);
-
-        for column in iter {
-            view.push((*column).to_vec());
-        }
+        let mut view = get_sub_view(&self.alternate_buffer, (0, 0), columns, rows);
 
         for i in 0..rows {
             for column in &view {
@@ -123,6 +182,44 @@ impl State {
                 execute!(stdout(), Print(character))?;
             }
         }
+
+        Ok(())
+    }
+
+    fn draw_buffer(&mut self) -> Result<()> {
+        // get current view window
+        let (columns, rows) : (usize, usize) = (self.dimensions.0.into(), self.dimensions.1.into());
+
+        let mut view: Vec<Vec<u8>> = get_sub_view(&self.alternate_buffer, (0, 0), columns, rows);
+        let old_buffer: Vec<Vec<u8>> = get_sub_view(&self.buffer, (0, 0), columns, rows);
+        
+        // grab view size parts of current and old buffer
+        // compare and print differnces
+        for (x, column) in view.clone().iter().enumerate() {
+            for (y, row) in column.iter().enumerate() {
+                if *row == old_buffer[x][y] {
+                    continue;
+                } else {
+                    let character: StyledContent<char> = self.style_map[old_buffer[x][y] as usize];
+                    execute!(
+                        stdout(),
+                        MoveToColumn(x as u16),
+                        MoveToRow(y as u16),
+                        Print(character)
+                    )?;
+                }
+            }
+        }
+
+        // update view buffer
+
+        self.alternate_buffer = self.buffer.clone();
+
+        execute!(
+            stdout(),
+            MoveToColumn(0),
+            MoveToRow(0)
+        )?;
 
         Ok(())
     }
